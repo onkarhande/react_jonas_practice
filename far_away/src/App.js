4ccbbc8d2defd2ba2ba1574883a7a37d1e96b564
Onkar Hande <onkarhande87@gmail.com>
2023-10-02 18:50:46 +0530

    Changes

--- far_away/src/App.js
+++ far_away/src/App.js
@@ -1,3 +1,7 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
 
 function App() {
   return (
@@ -21,13 +25,30 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list"></div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map(item => <Item item={item}></Item>)}
+      </ul>
+    </div>
+
+  );
 }
+
 function Stats() {
   return (
     <footer className="stats">
       <em>👜 You have X items on your list, and you have already packed X (X%)</em>
     </footer>
+  )
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>{item.quantity} {item.description}</span>
+      <button>❌</button>
+    </li>
   )
 }
 
