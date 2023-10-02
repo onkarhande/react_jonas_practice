a86c65031dc445e7d275cda9215a9231c8400a1e
Onkar Hande <onkarhande87@gmail.com>
2023-10-02 16:51:22 +0530

    Changes

---
+++ far_away/src/App.js
@@ -0,0 +1,34 @@
+
+function App() {
+  return (
+    <div className="App">
+      <Logo></Logo>
+      <Form></Form>
+      <PackingList></PackingList>
+      <Stats></Stats>
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 👜</h1>;
+}
+
+function Form() {
+  return (<div className="add-form">
+    <h3>What do you need for your 😍 trip</h3>
+  </div>)
+}
+
+function PackingList() {
+  return <div className="list"></div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜 You have X items on your list, and you have already packed X (X%)</em>
+    </footer>
+  )
+}
+
+export default App;
