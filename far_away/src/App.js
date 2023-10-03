b2b2545ab98e55cb350324a4c77af48a2a0a1b5c
Onkar Hande <onkarhande87@gmail.com>
2023-10-03 08:40:37 +0530

    Changes

--- far_away/src/App.js
+++ far_away/src/App.js
@@ -1,6 +1,6 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
 ];
 
 function App() {
@@ -19,37 +19,61 @@ function Logo() {
 }
 
 function Form() {
-  return (<div className="add-form">
-    <h3>What do you need for your 😍 trip</h3>
-  </div>)
+
+  function handleSubmit(e){
+    e.preventDefault();
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => {
+          return (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          );
+        })}
+      </select>
+      <input type="text"></input>
+      <button>Add</button>
+    </form>
+  );
 }
 
 function PackingList() {
   return (
     <div className="list">
       <ul>
-        {initialItems.map(item => <Item item={item}></Item>)}
+        {initialItems.map((item) => (
+          <Item item={item}></Item>
+        ))}
       </ul>
     </div>
-
   );
 }
 
 function Stats() {
   return (
     <footer className="stats">
-      <em>👜 You have X items on your list, and you have already packed X (X%)</em>
+      <em>
+        👜 You have {initialItems.length} items on your list, and you have
+        already packed X (X%)
+      </em>
     </footer>
-  )
+  );
 }
 
 function Item({ item }) {
   return (
     <li>
-      <span style={item.packed ? { textDecoration: "line-through" } : {}}>{item.quantity} {item.description}</span>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
       <button>❌</button>
     </li>
-  )
+  );
 }
 
 export default App;
